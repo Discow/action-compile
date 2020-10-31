1c4190a6d6346f990fd7fa434f8a9279df223f9a
dashing-designer <jsmail666666@gmail.com>
2020-10-31 13:33:54 +0800

    update

--- ubuntu-end.sh
+++ ubuntu-end.sh
@@ -1,15 +1,2 @@
 # This script runs after SSH in Ubuntu instances
-# Pack and upload files
-if [ ! -d ~/build/output ] || [ -f /tmp/stop ] ; then
-printf "No file uploads. Skipping"
-else
-cd ~/build
-tar -cvf output.tar output
-xz output.tar
-xz compile.log
-curl -sL https://git.io/file-transfer | sh
-./transfer gof ~/build/output.tar.xz
-./transfer wet ~/build/output.tar.xz
-./transfer gof ~/build/compile.log.xz
-./transfer wet ~/build/compile.log.xz
-fi
+#
--- ubuntu-start.sh
+++ ubuntu-start.sh
@@ -3,8 +3,4 @@
 ## Example ##
 
 # Setting the time zone
-#sudo timedatectl set-timezone "Asia/Shanghai"
-
-# Install the tools you need to use
-#sudo apt update
-#sudo apt install -y neofetch
+sudo timedatectl set-timezone "Asia/Shanghai"
