7f8ffbdf6b981e855c95a312147339e8806d63c8
dashing-designer <jsmail666666@gmail.com>
2020-11-03 18:06:38 +0800

    update ubuntu-end.sh

--- ubuntu-end.sh
+++ ubuntu-end.sh
@@ -7,7 +7,9 @@ cd ~/build
 tar -cvf output.tar output
 xz output.tar
 xz compile.log
-curl -sL https://git.io/file-transfer | sh
+# curl -sL https://git.io/file-transfer | sh
+wget https://github.com/Mikubill/transfer/releases/download/v0.4.8-beta/transfer_0.4.8-beta_linux_amd64.tar.gz
+tar -zxvf transfer_0.4.8-beta_linux_amd64.tar.gz
 ./transfer gof ~/build/output.tar.xz
 ./transfer wet ~/build/output.tar.xz
 ./transfer gof ~/build/compile.log.xz
