866b85100daa268c9e3a3d80c4a1964e95a2604b
SUPERPCOW <73215520+SUPERPCOW@users.noreply.github.com>
2020-11-15 17:58:40 +0800

    Update ubuntu-start.sh

--- ubuntu-start.sh
+++ ubuntu-start.sh
@@ -3,12 +3,24 @@
 ## Example ##
 
 # Setting the time zone
-#sudo timedatectl set-timezone "Asia/Shanghai"
+sudo timedatectl set-timezone "Asia/Shanghai"
+
+# Clean up
+docker rmi `docker images -q`
+sudo rm -rf /usr/share/dotnet /etc/mysql /etc/php
+sudo -E apt-get -y purge azure-cli ghc* zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql* php*
+sudo -E apt-get -y autoremove --purge
+sudo -E apt-get clean
 
 # Install the tools you need to use
-#sudo apt update
-#sudo apt install -y neofetch
+sudo -E apt-get update
+sudo -E apt-get -y install liblz4-tool
+#sudo -E apt-get -y install neofetch
 
 # Download the armbian tool
 cd ~
 git clone https://github.com/armbian/build
+
+# Download OrangePi_Build
+git clone https://github.com/orangepi-xunlong/OrangePi_Build.git
+
