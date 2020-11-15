e5d173539b5ebe39eb3e93330bd5b0feb5d5b440
SUPERPCOW <73215520+SUPERPCOW@users.noreply.github.com>
2020-11-16 05:51:14 +0800

    Update ubuntu-end.sh

--- ubuntu-end.sh
+++ ubuntu-end.sh
@@ -5,14 +5,14 @@ printf "No file uploads. Skipping"
 else
 cd ~/build
 sudo tar -cvpf output.tar output
-xz output.tar
+sudo xz output.tar
 sha1sum output.tar.xz
 sha1sum compile.log
 # curl -sL https://git.io/file-transfer | sh
 wget https://github.com/Mikubill/transfer/releases/download/v0.4.8-beta/transfer_0.4.8-beta2_linux_amd64.tar.gz
 tar -zxvf transfer_0.4.8-beta2_linux_amd64.tar.gz
-./transfer gof ~/build/output.tar.xz
+sudo ./transfer gof ~/build/output.tar.xz
 #./transfer wet ~/build/output.tar.xz
-./transfer gof ~/build/compile.log
+sudo ./transfer gof ~/build/compile.log
 #./transfer wet ~/build/compile.log.xz
 fi
