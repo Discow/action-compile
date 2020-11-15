84d9b732c68cfb63cf2feda73eaac2c13ea95f86
SUPERPCOW <73215520+SUPERPCOW@users.noreply.github.com>
2020-11-15 12:59:10 +0800

    Update ubuntu-end.sh

--- ubuntu-end.sh
+++ ubuntu-end.sh
@@ -6,12 +6,13 @@ else
 cd ~/build
 tar -cvf output.tar output
 xz output.tar
-xz compile.log
+sha1sum output.tar.xz
+sha1sum compile.log
 # curl -sL https://git.io/file-transfer | sh
-wget https://github.com/Mikubill/transfer/releases/download/v0.4.8-beta/transfer_0.4.8-beta_linux_amd64.tar.gz
-tar -zxvf transfer_0.4.8-beta_linux_amd64.tar.gz
+wget https://github.com/Mikubill/transfer/releases/download/v0.4.8-beta/transfer_0.4.8-beta2_linux_amd64.tar.gz
+tar -zxvf transfer_0.4.8-beta2_linux_amd64.tar.gz
 ./transfer gof ~/build/output.tar.xz
-./transfer wet ~/build/output.tar.xz
-./transfer gof ~/build/compile.log.xz
-./transfer wet ~/build/compile.log.xz
+#./transfer wet ~/build/output.tar.xz
+./transfer gof ~/build/compile.log
+#./transfer wet ~/build/compile.log.xz
 fi
